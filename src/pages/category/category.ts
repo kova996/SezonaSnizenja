c6394abe2ffeb135c0ff3c76b8b60a3d1d7a4f74
Alen Bašić <alenbas@outlook.com>
2018-01-13 21:17:58 +0100

    styled login page

--- src/pages/category/category.ts
+++ src/pages/category/category.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CategoryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +8,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor() {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoryPage');
-  }
+ pictures : any = {
+   
+ }
 
 }
