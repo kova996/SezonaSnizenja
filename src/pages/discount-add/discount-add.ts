96f87727539e5af169a00c22e54824e572c853bd
Alen Bašić <alenbas@outlook.com>
2018-02-18 20:00:01 +0100

    changes in image data

--- src/pages/discount-add/discount-add.ts
+++ src/pages/discount-add/discount-add.ts
@@ -16,7 +16,7 @@ export class DiscountAddPage {
   oldPrice = 0;
   discount = 0;
   imageData = "";
-  storage = firebase.storage().ref();
+  storage = firebase.storage().ref("pictures");
 
   options : CameraOptions = {
     quality: 100,
@@ -33,19 +33,24 @@ export class DiscountAddPage {
   }
 
   onImageTaken(){
-    this.camera.getPicture(this.options).then(data => {this.imageData = data;})
-    .catch(reason => {alert(reason)});
+    this.camera.getPicture(this.options).then(
+      data => {
+        this.imageData = data;
+        this.storage.putString(data, "data_url").then(()=> {alert("Uspilo")})
+        .catch(err => {alert(err.message)});
+      })
+    .catch(reason => {alert(reason.message)});
   }
 
   onSubmit(form : NgForm){
-    alert("Ušlo");
-    let filename = "test";
-    const imageRef = this.storage.child("images");
-    imageRef.putString(this.imageData,firebase.storage.StringFormat.DATA_URL).then(
-      response => alert(response),
-      err => {console.log("Ovde: " + err)})
-      .catch(err => alert(err));
-    alert("Kraj");
+
+    // const imageRef = this.storage.child("images");
+    // var message = '5b6p5Y-344GX44G-44GX44Gf77yB44GK44KB44Gn44Go44GG77yB';
+    // imageRef.putString(message,'base64url').then(
+    //   response => alert("Uspilo"),
+    //   err => {console.log("Ovde: " + err)})
+    //   .catch(err => alert(err.message));
+    // alert("Kraj");
   }
 
   changeOld(event : any){
